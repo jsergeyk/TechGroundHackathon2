a5db2e7dfd85539671bd838ad20616ea612b3185
SVKalchenko <jsergeyk@gmail.com>
2023-11-10 12:45:26 +0200

    Added entities

--- src/main/java/com/hackathon/entity/Attribute.java
+++ src/main/java/com/hackathon/entity/Attribute.java
@@ -3,7 +3,7 @@ package com.hackathon.entity;
 import jakarta.persistence.*;
 
 @Entity
-public class Infrastructura {
+public class Attribute {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id", nullable = false)
@@ -11,12 +11,6 @@ public class Infrastructura {
     @Basic
     @Column(name = "name", nullable = false, length = 255)
     private String name;
-    @Basic
-    @Column(name = "availability")
-    private Boolean availability;
-    @Basic
-    @Column(name = "type", nullable = false)
-    private Long type;
 
     public Long getId() {
         return id;
@@ -34,32 +28,15 @@ public class Infrastructura {
         this.name = name;
     }
 
-    public Long getType() {
-        return type;
-    }
-
-    public void setType(Long type) {
-        this.type = type;
-    }
-
-    public Boolean getAvailability() {
-        return availability;
-    }
-
-    public void setAvailability(Boolean availability) {
-        this.availability = availability;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Infrastructura that = (Infrastructura) o;
+        Attribute attribute = (Attribute) o;
 
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (availability != null ? !availability.equals(that.availability) : that.availability != null) return false;
+        if (id != null ? !id.equals(attribute.id) : attribute.id != null) return false;
+        if (name != null ? !name.equals(attribute.name) : attribute.name != null) return false;
 
         return true;
     }
@@ -68,7 +45,6 @@ public class Infrastructura {
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
         result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (availability != null ? availability.hashCode() : 0);
         return result;
     }
 }
