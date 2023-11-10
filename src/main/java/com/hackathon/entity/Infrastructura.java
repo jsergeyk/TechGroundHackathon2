d502dbf8a021377e9bff823873ed14eb18f41624
SVKalchenko <jsergeyk@gmail.com>
2023-11-10 11:09:45 +0200

    Init commit database MySql

---
+++ src/main/java/com/hackathon/entity/Infrastructura.java
@@ -0,0 +1,63 @@
+package com.hackathon.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Infrastructura {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Basic
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+    @Basic
+    @Column(name = "availabily", nullable = true, length = 45)
+    private String availabily;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvailabily() {
+        return availabily;
+    }
+
+    public void setAvailabily(String availabily) {
+        this.availabily = availabily;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Infrastructura that = (Infrastructura) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (availabily != null ? !availabily.equals(that.availabily) : that.availabily != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (availabily != null ? availabily.hashCode() : 0);
+        return result;
+    }
+}
